6f31569ca7a749e314f77b65a4d69a233b348ce2
Maxim V. Potapov <mpotapov@unison.com>
2013-03-04 14:35:59 +0400

    macx: flags for static linking with qt4

--- platform/MacOSX.cmake
+++ platform/MacOSX.cmake
@@ -1,8 +1,9 @@
 # Mac OS X specific code
+set(MacOSX true)
 
 # comatibility with older systems
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.6")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.6")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.6 -fvisibility=hidden")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.6 -fvisibility=hidden -fvisibility-inlines-hidden")
 set(CMAKE_LFLAGS "${CMAKE_LFLAGS} -mmacosx-version-min=10.6")
 
 # on Mac OS X app type should be MACOSX_BUNDLE

9881b7ad3fd5ceea6a6f7e0cd7f3d1992d90b1d9
Maxim V. Potapov <mpotapov@unison.com>
2013-03-07 00:14:44 +0400

    macx: address sanitizer (clang 3.1+)

--- platform/MacOSX.cmake
+++ platform/MacOSX.cmake
@@ -2,9 +2,9 @@
 set(MacOSX true)
 
 # comatibility with older systems
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.6 -fvisibility=hidden")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.6 -fvisibility=hidden -fvisibility-inlines-hidden")
-set(CMAKE_LFLAGS "${CMAKE_LFLAGS} -mmacosx-version-min=10.6")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer -mmacosx-version-min=10.6 -fvisibility=hidden")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer -mmacosx-version-min=10.6 -fvisibility=hidden -fvisibility-inlines-hidden")
+set(CMAKE_LFLAGS "${CMAKE_LFLAGS} -fsanitize=address -mmacosx-version-min=10.6")
 
 # on Mac OS X app type should be MACOSX_BUNDLE
 set(APP_TYPE MACOSX_BUNDLE)

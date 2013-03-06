67f5d26867c0e86fa4ecbca5951223bf40d5fdbf
Maxim V. Potapov <mpotapov@unison.com>
2013-03-06 15:07:28 +0400

    clang static analyzer and their flags

--- CommonCompilerFlags.cmake
+++ CommonCompilerFlags.cmake
@@ -10,11 +10,19 @@ if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 else()
     # using Clang, GNU or Intel
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -fPIC")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -fPIC")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -fPIC -fcatch-undefined-behavior")
 endif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 # special definitions
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DQT_NO_DEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DQT_NO_DEBUG")
+
+# clang static analyzer
+if(STATIC_ANALYZER)
+    set(CMAKE_C_FLAGS "--analyze -S -Qunused-arguments")
+    set(CMAKE_CXX_FLAGS "--analyze -S -Qunused-arguments")
+    set(CMAKE_C_FLAGS_RELEASE "")
+    set(CMAKE_CXX_FLAGS_RELEASE "")
+endif()
 
 # Output compilers settings
 include(cmake/VerboseCompilerMessage.cmake)

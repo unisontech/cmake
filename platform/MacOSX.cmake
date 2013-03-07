345b2ca98a4f65ba6ad0c69b484baf605d861442
Maxim V. Potapov <mpotapov@unison.com>
2013-03-07 11:28:51 +0400

    typo

--- platform/MacOSX.cmake
+++ platform/MacOSX.cmake
@@ -12,7 +12,7 @@ set(BUILD_SHARED_LIBS false)
 
 # http://clang.llvm.org/docs/AddressSanitizer.html
 # Static linking is not supported.
-set(BUILD_SHARED_LIBS)
+if(BUILD_SHARED_LIBS)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
     set(CMAKE_LFLAGS "${CMAKE_LFLAGS} -fsanitize=address")

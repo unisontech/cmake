c1d81942b37cea7abc6a278aa0abaa33303ea90a
Vyacheslav Blinov <vblinov@unison.com>
2013-03-10 00:28:20 +0400

    Advanced warnings for windows compiler

--- CommonCompilerFlags.cmake
+++ CommonCompilerFlags.cmake
@@ -3,9 +3,17 @@ set(COMMON_COMPILER_FLAGS_CMAKE true)
 
 # compilers
 if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    # using Visual Studio C++, use -W3
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -WX -W 3 /EHsc")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -WX -W 3 /EHsc")
+    # using Visual Studio C++, use -W3 or -W4
+    # FIXME: add following warnings later or find a way to use -W4:
+    #  -we4702
+    set(MSVC_W4_WARNINGS "-we4100 -we4101 -we4189 -we4700 -we4701 -we4705 -we4706 -we4710 -we4722 -we4723")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -WX -W3 ${MSVC_W4_WARNINGS} /EHsc")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -WX -W3 ${MSVC_W4_WARNINGS} /EHsc")
+    if(MSVC12)
+        # With Visual Studio 2012 add /sdl flag for treating security warnings as errors
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /sdl")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /sdl")
+    endif()
 else()
     # using Clang, GNU or Intel
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -fPIC")

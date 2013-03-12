f8958d98cfc4db9de535593f43d4a74e0a418ac5
Vyacheslav Blinov <vblinov@unison.com>
2013-03-12 17:07:12 +0400

    Don't build shared libraries on Linux

--- platform/Linux.cmake
+++ platform/Linux.cmake
@@ -1,7 +1,6 @@
 # linux-specific goes here
 set(LINUX true)
+set(BUILD_SHARED_LIBRARIES false)
 if(SANITIZER)
     set(BUILD_SHARED_LIBRARIES true)
-else()
-    set(BUILD_SHARED_LIBRARIES false)
 endif()
